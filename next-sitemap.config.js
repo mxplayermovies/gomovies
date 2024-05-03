426b68967903f25e5cd4e0f18a7aa7fd0d07a2b7
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-03 08:56:01 +0530

    sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://uwatchfreeonline.vercel.app/',
+    siteUrl: 'https://watchonlinemovies.vercel.app/',
     changefreq: 'daily',
     priority: 0.90,
     generateRobotsTxt: true,
