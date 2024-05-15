83f3a865ff5fc5ed2a31c20181c0de984adebd63
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-15 11:04:19 +0530

    RE UP DATE

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleTagManager from '../components/GoogleTagManager';
+import GoogleTagManager from '../../components/GoogleTagManager';
 
 const uwatchfreeSchema = JSON.stringify([
   {
