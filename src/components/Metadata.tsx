fdeb42c94c01b77145627e9603be7aaf6aa6c9ae
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-15 14:26:38 +0530

    COME ON GOD

---
+++ src/components/Metadata.tsx
@@ -0,0 +1,11 @@
+export interface Metadata {
+  title: string;
+  description: string;
+  ogImage?: string;
+  url: string;
+  keywords: string;
+  viewportSettings?: string;
+}
+
+// Export the Metadata interface
+export default Metadata;
