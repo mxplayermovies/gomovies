c210ea5283d37abc7f0f5819f78d6f9e6db61c2c
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-16 04:47:20 +0530

    fuck

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -16,47 +16,45 @@ import {
 } from 'next-share';
 
 const ShareButtons = () => {
-  const shareUrl = typeof window !== 'undefined' ? window.location.href : ''; // Use current window location as share URL
-  const shareQuote = 'Watch Online Movies™ - For Movies & TV Show.';
+  const shareUrl = 'https://github.com/next-share'; // Example share URL
   const shareTitle = 'Watch Online Movies™ - For Movies & TV Show.';
-  const shareSubject = 'Watch Online Latest Movies !!!';
-  const shareBody = 'Check the Link Now';
+  const shareHashtag = '#drtrailer';
+  const shareSubject = 'Watch Online Movies™ - For Movies & TV Show.';
+  const shareBody = 'Check out this cool share button!';
 
   return (
     <div className="bg-transparent flex gap-2 justify-center p-2">
       {/* Facebook Share Button */}
-      <FacebookShareButton url={shareUrl} quote={shareQuote} hashtag="#drtrailer">
-        <FacebookIcon size={36} round />
+      <FacebookShareButton url={shareUrl} quote={shareTitle} hashtag={shareHashtag}>
+        <FacebookIcon size={32} round />
       </FacebookShareButton>
 
       {/* Telegram Share Button */}
       <TelegramShareButton url={shareUrl} title={shareTitle}>
-        <TelegramIcon size={36} round />
+        <TelegramIcon size={32} round />
       </TelegramShareButton>
 
       {/* Twitter Share Button */}
       <TwitterShareButton url={shareUrl} title={shareTitle}>
-        <TwitterIcon size={36} round />
+        <TwitterIcon size={32} round />
       </TwitterShareButton>
 
       {/* Whatsapp Share Button */}
-      <WhatsappShareButton url={shareUrl} title={shareTitle} separator=":: ">
-        <WhatsappIcon size={36} round />
+      <WhatsappShareButton url={shareUrl} title={shareTitle} separator="::">
+        <WhatsappIcon size={32} round />
       </WhatsappShareButton>
 
       {/* Linkedin Share Button */}
       <LinkedinShareButton url={shareUrl}>
-        <LinkedinIcon size={36} round />
+        <LinkedinIcon size={32} round />
       </LinkedinShareButton>
 
       {/* Email Share Button */}
       <EmailShareButton url={shareUrl} subject={shareSubject} body={shareBody}>
-        <EmailIcon size={36} round />
+        <EmailIcon size={32} round />
       </EmailShareButton>
     </div>
   );
 };
 
 export default ShareButtons;
-
-
