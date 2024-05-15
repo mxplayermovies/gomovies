182dfc97ae444a48ee5fc62cabdea2df6c74d132
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-15 14:51:18 +0530

    KEEP IT GOING

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -1,11 +1,22 @@
-export interface Metadata {
-  title: string;
-  description: string;
-  ogImage?: string;
-  url: string;
-  keywords: string;
-  viewportSettings?: string;
-}
+// export interface Metadata {
+//   title: string;
+//   description: string;
+//   ogImage?: string;
+//   url: string;
+//   keywords: string;
+//   viewportSettings?: string;
+// }
 
-// Export the Metadata interface
-export default Metadata;
+// export default Metadata;
+
+import Metadata from '@/components/Metadata';
+
+// Now you can use Metadata in your code
+const metadata: Metadata = {
+  title: 'Watch Online Movies™ - The Best Movies Platform HD Movies.',
+  description: 'Watch free Movie, & TV shows online on any device. We offer streaming on any Platform. Watch now !!!',
+  ogImage: 'https://watchonlinemovies.vercel.app/og_image.jpg',
+  url: 'https://watchonlinemovies.vercel.app/',
+  keywords: 'watch online movie, watch online movie latest movie, free movie, films, free movie online, free movie online, online movie, watch online movie free sites, watch free movie online free, free online movie, watch movie online, watch online movie, watch movie free online, online watch movie, free new movie online, watch movie online free, movie 2 watch, free full movie, online movie watch, movie online watch, online movie for free, online watch movie, 2024 Latest movie, 2024 latest movie online',
+  viewportSettings: 'width=device-width, initial-scale=1.0',
+};
