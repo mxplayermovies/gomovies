de2c0c7d8cb68173d83af0d3801cab1ee0031785
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-15 15:50:14 +0530

    UPSATED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -159,7 +159,101 @@ export default function RootLayout ({
   return (
     <html lang='en'>
       <body className={inter.className}>
-       
+      <link
+          rel='sitemap'
+          type='application/xml'
+          title='Sitemap'
+          href='https://watchonlinemovies.vercel.app/sitemap.xml'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='googlebot' content='index,follow' />
+        <meta name='revisit-after' content='1 days' />
+        <meta
+          name='robots'
+          content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+        />
+        <meta
+          name='keywords'
+          content='uwatchfree, uwatchfree latest movie, free movie, films, free movie online, free movie online, online movie, watch online movie free sites, watch free movie online free, free online movie, watch movie online, watch online movie, watch movie free online, online watch movie, free new movie online, watch movie online free, movie 2 watch, free full movie, online movie watch, movie online watch, online movie for free, online watch movie, 2024 Latest movie, 2024 Latest movie online'
+        />
+        <meta
+          name='description'
+          content='Watch free Movie, & TV shows online on any device. We offer streaming on any Platform. Watch now !!!'
+        />
+        <link rel='canonical' href='https://watchonlinemovies.vercel.app/' />
+        <meta property='og:locale' content='en_US' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:title'
+          content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
+        />
+        <meta
+          property='og:description'
+          content='Watch free Movie, & TV shows online on any device. We offer streaming on any Platform. Watch now !!!'
+        />
+        <meta
+          property='og:url'
+          content='https://watchonlinemovies.vercel.app/'
+        />
+        <meta
+          property='og:site_name'
+          content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
+        />
+        <meta
+          property='og:image'
+          content='https://watchonlinemovies.vercel.app/og_image.jpg'
+        />
+        <meta property='og:image:width' content='1280' />
+        <meta property='og:image:height' content='720' />
+        <meta property='og:image:type' content='image/webp' />
+        <meta
+          name='application-name'
+          content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
+        />
+
+        <meta
+          property='article:modified_time'
+          content='2024-01-01T13:13:13+00:00'
+        />
+        <link
+          rel='sitemap'
+          type='application/xml'
+          title='Sitemap'
+          href='https://watchonlinemovies.vercel.app/sitemap.xml'
+        />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta
+          name='google-site-verification'
+          content='4dFu4PUk1pc1IYqU6Brt84akCwNxaoUpKSO3gDW0kJ0'
+        />
+        <meta
+          name='facebook-domain-verification'
+          content='du918bycikmo1jw78wcl9ih6ziphd7'
+        />
+        <meta
+          name='dailymotion-domain-verification'
+          content='dm0zffs8dj8pcb3gd'
+        />
+        <meta name='monetag' content='5c1338da4436ca30815b74198d2dfe20' />
           
           <script
             type='application/ld+json'
@@ -169,7 +263,7 @@ export default function RootLayout ({
             type='application/ld+json'
             dangerouslySetInnerHTML={{ __html: rankMathSchema }}
           />
-          <cript
+          {/* <cript
             dangerouslySetInnerHTML={{
               __html: `
             (function (w, d, s, id) {
@@ -184,7 +278,7 @@ export default function RootLayout ({
             webpushr('setup', { 'key': 'BFiNtErWPSlY1EdDYECv3rzPp2d5bVs46-O6I4iILsKtA2USCyBEakKcLs7yooH4Gj36tIvYtQtpZ0qZVfhHyAU' });
           `
             }}
-          />
+          /> */}
      
         <Script src='../../propler/ads.js' defer />
         <GoogleTagManager gtmId={gtmId} />
