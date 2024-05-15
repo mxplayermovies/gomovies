ac55911ab589676bdb81444625a7bee5b0f10a0f
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-15 14:14:45 +0530

    update god please

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import { Metadata as NextMetadata } from 'next';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -11,6 +11,8 @@ import GoogleTagManager from '@/components/GoogleTagManager';
 const inter = Inter({ subsets: ["latin"] });
 
 const gtmId = 'G-REYHDZZ5K8'; 
+
+
 
 const uwatchfreeSchema = JSON.stringify([
   {
@@ -141,9 +143,27 @@ const rankMathSchema = JSON.stringify({
 })
 
 export const metadata: Metadata = {
-  title: "Watch Online Movies™",
-  description: " The Best Movies Platform HD Movies.",
+  title: 'Watch Online Movies™ - The Best Movies Platform HD Movies.',
+  description: 'Watch free Movie, & TV shows online on any device. We offer streaming on any Platform. Watch now !!!',
+  ogImage: 'https://watchonlinemovies.vercel.app/og_image.jpg',
+  url: 'https://watchonlinemovies.vercel.app/',
+  keywords: 'watch online movie, watch online movie latest movie, free movie, films, free movie online, free movie online, online movie, watch online movie free sites, watch free movie online free, free online movie, watch movie online, watch online movie, watch movie free online, online watch movie, free new movie online, watch movie online free, movie 2 watch, free full movie, online movie watch, movie online watch, online movie for free, online watch movie, 2024 Latest movie, 2024 latest movie online',
+  viewportSettings: 'width=device-width, initial-scale=1.0',
 };
+
+export interface Metadata {
+  title: string;
+  description: string;
+  ogImage?: string; // Optional ogImage property
+  url: string;
+  keywords: string;
+  viewportSettings?: string;
+}
+
+// export const metadata: Metadata = {
+//   title: "Watch Online Movies™",
+//   description: " The Best Movies Platform HD Movies.",
+// };
 
 export default function RootLayout({
   children,
@@ -154,169 +174,51 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <Head>
-          <title>
-            {' '}
-            Watch Online Movies™ - The Best Movies Platform HD Movies.{' '}
-          </title>
-          <link
-            rel='sitemap'
-            type='application/xml'
-            title='Sitemap'
-            href='https://watchonlinemovies.vercel.app/sitemap.xml'
-          />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
-          <link
-            rel='icon'
-            type='image/x-icon'
-            href='/favicon.ico'
-          />
-          <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/apple-touch-icon.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/favicon-32x32.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/favicon-16x16.png'
-          />
-          <link rel='manifest' href='/site.webmanifest' />
-          <meta name='googlebot' content='index,follow' />
-          <meta name='revisit-after' content='1 days' />
-          <meta
-            name='robots'
-            content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
-          />
-          <meta
-            name='keywords'
-            content='uwatchfree, uwatchfree latest movie, free movie, films, free movie online, free movie online, online movie, watch online movie free sites, watch free movie online free, free online movie, watch movie online, watch online movie, watch movie free online, online watch movie, free new movie online, watch movie online free, movie 2 watch, free full movie, online movie watch, movie online watch, online movie for free, online watch movie, 2024 Latest movie, 2024 Latest movie online'
-          />
-          <meta
-            name='description'
-            content='Watch free movie, TV shows & Sports online on any device. We offer streaming on any Platform. Watch now !!!'
-          />
-          <link rel='canonical' href='https://watchonlinemovies.vercel.app/' />
-          <meta property='og:locale' content='en_US' />
-          <meta property='og:type' content='website' />
-          <meta
-            property='og:title'
-            content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
-          />
-          <meta
-            property='og:description'
-            content='Watch free movie, TV shows & Sports online on any device. We offer streaming on any Platform. Watch now !!!'
-          />
-          <meta
-            property='og:url'
-            content='https://watchonlinemovies.vercel.app/'
-          />
-          <meta
-            property='og:site_name'
-            content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
-          />
-          <meta
-            property='og:image'
-            content='https://watchonlinemovies.vercel.app/og_image.jpg'
-          />
-          <meta property='og:image:width' content='1280' />
-          <meta property='og:image:height' content='720' />
-          <meta property='og:image:type' content='image/webp' />
-          <meta
-            name='application-name'
-            content='Watch Online Movies™ - The Best Movies Platform HD Movies.'
-          />
-          
-          <link
-            rel="alternate"
-            hrefLang="en-us"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-gb"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-ca"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-au"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-se"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-fr"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-dk"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="en-no"
-            href="/"
-          />
-          <link
-            rel="alternate"
-            hrefLang="x-default"
-            href="/"
-          />
-
-          <meta
-            property='article:modified_time'
-            content='2024-01-01T13:13:13+00:00'
-          />
-          <link
-            rel='sitemap'
-            type='application/xml'
-            title='Sitemap'
-            href='https://watchonlinemovies.vercel.app/sitemap.xml'
-          />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta
-            name='google-site-verification'
-            content='4dFu4PUk1pc1IYqU6Brt84akCwNxaoUpKSO3gDW0kJ0'
-          />
-          <meta
-            name='facebook-domain-verification'
-            content='du918bycikmo1jw78wcl9ih6ziphd7'
-          />
-          <meta
-            name='dailymotion-domain-verification'
-            content='dm0zffs8dj8pcb3gd'
-          />
-          <meta name='monetag' content='5c1338da4436ca30815b74198d2dfe20' />
-          <script
-            type='application/ld+json'
-            dangerouslySetInnerHTML={{ __html: uwatchfreeSchema }}
-          />
-          <script
-            type='application/ld+json'
-            dangerouslySetInnerHTML={{ __html: rankMathSchema }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        <title>{metadata.title}</title>
+        <link rel='sitemap' type='application/xml' title='Sitemap' href='https://watchonlinemovies.vercel.app/sitemap.xml' />
+        <meta name="viewport" content={metadata.viewportSettings} />
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='googlebot' content='index,follow' />
+        <meta name='revisit-after' content='1 days' />
+        <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+        <meta name='keywords' content={metadata.keywords} />
+        <meta name="description" content={metadata.description} />
+        <link rel="canonical" href={metadata.url} />
+        <meta property='og:locale' content='en_US' />
+        <meta property='og:type' content='website' />
+        <meta name='og:title' content={metadata.title} />
+        <meta name="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.url} />
+        <meta name='og:site_name' content={metadata.title} />
+        {metadata.ogImage && <meta property="og:image" content={metadata.ogImage} />}
+        <meta property='og:image:width' content='1280' />
+        <meta property='og:image:height' content='720' />
+        <meta property='og:image:type' content='image/webp' />
+        <meta name='application-name' content={metadata.title} />
+        <link rel='alternate' hrefLang='en-us' href='/' />
+        <link rel='alternate' hrefLang='en-gb' href='/' />
+        <link rel='alternate' hrefLang='en-ca' href='/' />
+        <link rel='alternate' hrefLang='en-au' href='/' />
+        <link rel='alternate' hrefLang='en-se' href='/' />
+        <link rel='alternate' hrefLang='en-fr' href='/' />
+        <link rel='alternate' hrefLang='en-dk' href='/' />
+        <link rel='alternate' hrefLang='en-no' href='/' />
+        <link rel='alternate' hrefLang='x-default' href='/' />
+        <meta property='article:modified_time' content='2024-01-01T13:13:13+00:00' />
+        <link rel='sitemap' type='application/xml' title='Sitemap' href='https://watchonlinemovies.vercel.app/sitemap.xml' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='google-site-verification' content='4dFu4PUk1pc1IYqU6Brt84akCwNxaoUpKSO3gDW0kJ0' />
+        <meta name='facebook-domain-verification' content='du918bycikmo1jw78wcl9ih6ziphd7' />
+        <meta name='dailymotion-domain-verification' content='dm0zffs8dj8pcb3gd' />
+        <meta name='monetag' content='5c1338da4436ca30815b74198d2dfe20' />
+        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: uwatchfreeSchema }} />
+        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: rankMathSchema }} />
+        <script dangerouslySetInnerHTML={{
+          __html: `
             (function (w, d, s, id) {
               if (typeof (w.webpushr) !== 'undefined') return;
               w.webpushr = w.webpushr || function () { (w.webpushr.q = w.webpushr.q || []).push(arguments) };
@@ -328,11 +230,8 @@ export default function RootLayout({
 
             webpushr('setup', { 'key': 'BFiNtErWPSlY1EdDYECv3rzPp2d5bVs46-O6I4iILsKtA2USCyBEakKcLs7yooH4Gj36tIvYtQtpZ0qZVfhHyAU' });
           `
-            }}
-          />
-       
-
-        </Head>
+        }} />
+      </Head>
       <Script src='../../propler/ads.js' defer />
       <GoogleTagManager gtmId={gtmId} />
         <ThemeProvider
