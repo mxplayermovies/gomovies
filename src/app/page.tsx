f9b12aeb340f97fc6d66de9830e6a84933a28baf
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-05-16 12:11:47 +0530

    ok now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,53 @@ export default async function Home() {
   return (
     <main>
       <title> Watch Online Movies™ - For Movies & TV Show. </title>
+      <link
+        rel='sitemap'
+        type='application/xml'
+        title='Sitemap'
+        href='https://watchonlinemovies.vercel.app/sitemap.xml'
+      />
+      <meta property='og:locale' content='en_US' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/site.webmanifest' />
+      <meta name='googlebot' content='index,follow' />
+      <meta name='revisit-after' content='1 days' />
+      <meta
+        name='robots'
+        content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+      />
+      <meta
+        name='keywords'
+        content='watch online movie, watch online latest movie, free movie, films, free movie online, free movie online, online movie, watch online movie free sites, watch free movie online free, free online movie, watch movie online, watch online movie, watch movie free online, online watch movie, free new movie online, watch movie online free, movie 2 watch, free full movie, online movie watch, movie online watch, online movie for free, online watch movie, 2024 Latest movie, 2024 Latest movie online'
+      />
+
+      <meta
+        name='description'
+        content='Watch free Movie, & TV shows online on any device. We offer streaming on any Platform. Watch now !!!'
+      />
+      <link rel='canonical' href='https://watchonlinemovies.vercel.app/' />
+      <meta
+        name='google-site-verification'
+        content='WQh7UdOVLh--PluVaU8U1m1IHrAPjaWmaItVOdek8tg'
+      />
        <meta
           property='og:image'
           content='https://watchonlinemovies.vercel.app/og_image.jpg'
